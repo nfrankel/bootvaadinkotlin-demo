bd277d922d7b0dc582c76cc2059df340e65dad8f
Nicolas Frankel <nicolas@frankel.ch>
2016-03-22 09:34:08 +0100

    Alternative way to add the lang attribute to html tag

--- src/main/kotlin/ch/frankel/blog/bootvaadinkotlin/AppConfiguration.kt
+++ src/main/kotlin/ch/frankel/blog/bootvaadinkotlin/AppConfiguration.kt
@@ -1,11 +1,13 @@
 package ch.frankel.blog.bootvaadinkotlin
 
-import javax.sql.*
+import com.vaadin.server.*
 import com.vaadin.spring.annotation.*
+import com.vaadin.spring.server.*
 import com.vaadin.ui.*
 import org.springframework.beans.factory.annotation.*
 import org.springframework.context.annotation.*
 import org.vaadin.spring.events.internal.ScopedEventBus
+import javax.sql.*
 
 @Configuration
 open class AppConfiguration {
@@ -33,4 +35,27 @@ open class AppConfiguration {
 
     @Bean
     open fun appEventBus() = ScopedEventBus.DefaultApplicationEventBus()
+
+    @Bean
+    open fun vaadinServlet() = CustomVaadinServlet({ event: SessionInitEvent ->
+
+        event.session.addBootstrapListener(object : BootstrapListener {
+
+            override fun modifyBootstrapFragment(response: BootstrapFragmentResponse) {
+                // NOP, this is for portlets etc
+            }
+
+            override fun modifyBootstrapPage(response: BootstrapPageResponse) {
+                response.document.child(0).attr("lang", "fr")
+            }
+        })
+    })
+}
+
+class CustomVaadinServlet(private val listener: (SessionInitEvent) -> Unit) : SpringVaadinServlet() {
+
+    override fun servletInitialized() {
+        super.servletInitialized();
+        service.addSessionInitListener(listener)
+    }
 }
